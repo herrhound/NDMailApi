28fd8612bbaaecfd5b2c7b4f0c567460ea7a1b0c
Alexandr Moroz <herrhound@gmail.com>
2014-05-26 23:33:14 -0700

    Printing out current port

--- src/main/scala/web/Web.scala
+++ src/main/scala/web/Web.scala
@@ -10,11 +10,7 @@ trait Web {
   this: Api with CoreActors with Core =>
 
   val host = system.settings.config.getString("NDMailApi.interface")
-  val alternativePort = system.settings.config.getInt("NDMailApi.port")
-  //val port = Option(System.getenv("PORT")).getOrElse(alternativePort).toString.toInt
-  //val port = scala.util.Properties.envOrElse("PORT", "80").toInt
-
   val port = Properties.envOrElse("PORT", "8080").toInt
-
+  println(port)
   IO(Http) ! Http.Bind(rootService, host, port)
 }
