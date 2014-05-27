e9aa2a8d9ca4581a239d77ac91ca7a425a36d152
Alexandr Moroz <herrhound@gmail.com>
2014-05-26 23:12:54 -0700

    Another try reading the port

--- src/main/scala/web/Web.scala
+++ src/main/scala/web/Web.scala
@@ -4,6 +4,7 @@ import akka.io.IO
 import spray.can.Http
 import api.Api
 import core.{CoreActors, Core}
+import util.Properties
 
 trait Web {
   this: Api with CoreActors with Core =>
@@ -11,7 +12,9 @@ trait Web {
   val host = system.settings.config.getString("NDMailApi.interface")
   val alternativePort = system.settings.config.getInt("NDMailApi.port")
   //val port = Option(System.getenv("PORT")).getOrElse(alternativePort).toString.toInt
-  val port = scala.util.Properties.envOrElse("PORT", "80").toInt
+  //val port = scala.util.Properties.envOrElse("PORT", "80").toInt
+
+  val port = Properties.envOrElse("PORT", "8080").toInt
 
   IO(Http) ! Http.Bind(rootService, host, port)
 }
