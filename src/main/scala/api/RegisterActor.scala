660d0b801f645e179090cee5aaa876d9b250c07d
Nikola Tonkev <nikola_tonkev@yahoo.com>
2014-09-15 22:50:55 -0400

    form post 1.6

--- src/main/scala/api/RegisterActor.scala
+++ src/main/scala/api/RegisterActor.scala
@@ -195,14 +195,15 @@ object RegisterActor extends NDApiLogging with NDApiUtil with  DefaultJsonFormat
     //pipeline{Post("https://accounts.google.com/o/oauth2/token", request)}
 
     //val formData = FormData(Map("grant_type" -> grant_type, "code" -> code, "client_id" -> client_id, "client_secret" -> client_secret, "redirect_uri" -> redirect_uri))
-    //val formData = FormData(Seq("grant_type" -> "authorization_code",
-    //  "code" -> code, "client_id" -> client_id, "client_secret" -> client_secret, "redirect_uri" -> redirect_uri)).asInstanceOf[HttpData.NonEmpty]
+    val formData = FormData(Seq("grant_type" -> "authorization_code",
+      "code" -> code, "client_id" -> client_id, "client_secret" -> client_secret, "redirect_uri" -> redirect_uri)).asInstanceOf[HttpData.NonEmpty]
     //val formData = FormData(Seq(("grant_type", "authorization_code"), ("code", code), ("client_id", client_id), ("client_secret", client_secret), ("redirect_uri", redirect_uri)))
 
-    //val httpData = HttpData(formData.asInstanceOf[HttpData.NonEmpty].toString())
+    //val raw = "grant_type=authorization_code&code=" + code.toString()+ "&client_id=" + client_id.toString()+ "&client_secret=" + client_secret.toString()+ "&redirect_uri=" + redirect_uri.toString()
+    val httpData = HttpData(formData.asInstanceOf[HttpData.NonEmpty].toString())
     //val entity = HttpEntity.NonEmpty(ContentType(MediaTypes.`application/x-www-form-urlencoded`), Some(formData).asInstanceOf[HttpData.NonEmpty])
 
-    val raw = "?grant_type=authorization_code&code=" + code.toString()+ "&client_id=" + client_id.toString()+ "&client_secret=" + client_secret.toString()+ "&redirect_uri=" + redirect_uri.toString()
+    val raw = "grant_type=authorization_code&code=" + code.toString()+ "&client_id=" + client_id.toString()+ "&client_secret=" + client_secret.toString()+ "&redirect_uri=" + redirect_uri.toString()
     val data = Some(raw)
     pipeline{
       //Post("https://accounts.google.com/o/oauth2/token").withEntity(entity)
