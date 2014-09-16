b8d5baa43c53a1dc8fe812ad652e4456622dcc91
Nikola Tonkev <nikola_tonkev@yahoo.com>
2014-09-16 00:25:29 -0400

    form post 1.7

--- src/main/scala/api/RegisterActor.scala
+++ src/main/scala/api/RegisterActor.scala
@@ -190,7 +190,7 @@ object RegisterActor extends NDApiLogging with NDApiUtil with  DefaultJsonFormat
         ~> decode(Deflate)
         ~> unmarshal[GoogleToken]
       )
-    //val request:GoogleTokenRequest = new GoogleTokenRequest(grant_type, code, client_id, client_secret, redirect_uri)
+    val request: GoogleTokenRequest = new GoogleTokenRequest(grant_type, code, client_id, client_secret, redirect_uri)
     //println("Request: "+request.toString())
     //pipeline{Post("https://accounts.google.com/o/oauth2/token", request)}
 
@@ -206,15 +206,8 @@ object RegisterActor extends NDApiLogging with NDApiUtil with  DefaultJsonFormat
     //val raw = "grant_type=authorization_code&code=" + code.toString()+ "&client_id=" + client_id.toString()+ "&client_secret=" + client_secret.toString()+ "&redirect_uri=" + redirect_uri.toString()
     //val data = Some(raw)
 
-    var urlParameters = Map[String, String]()
-    urlParameters += ("client_id" -> client_id)
-    urlParameters += ("client_secret" -> client_secret)
-    urlParameters += ("redirect_uri" -> redirect_uri)
-    urlParameters += ("code" -> code)
-    urlParameters += ("grant_type" -> grant_type)
-
     pipeline{
-      Post("https://accounts.google.com/o/oauth2/token", formData)
+      Post("https://accounts.google.com/o/oauth2/token", request)
       //Post("https://accounts.google.com/o/oauth2/token", data)
     }
   }
