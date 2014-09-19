4f2df5cac2c7c7330ec16bd570ce8c3db86ba62d
Nikola Tonkev <nikola_tonkev@yahoo.com>
2014-09-19 16:08:30 -0400

    form post 1.7.6 GoogleToken Returned to client

--- src/main/scala/api/RegisterService.scala
+++ src/main/scala/api/RegisterService.scala
@@ -78,13 +78,13 @@ class RegisterService(system: ActorSystem, registering: ActorRef)(implicit conte
 
             val result = GetGoogleAccessToken(code).onComplete {
               case Success(token) => {
-                //println("Google access token : " + token)
+                println("Success : " + token)
                 //val stkn = new NDApiResponse[GoogleToken](ErrorStatus.None, "Authenticated", token)
                 //stkn
                 token
               }
               case Failure(ex) => {
-                //println("Failure : " + ex.toString())
+                println("Failure : " + ex.toString())
                 //val ftkn = new NDApiResponse[GoogleToken](ErrorStatus.NotAutorized, ex.toString(), new GoogleToken("",0,"","",""))
                 //ftkn
                 null
